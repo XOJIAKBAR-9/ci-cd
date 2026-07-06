0b1467a5047ffe545d2b6a5f858b6021d2283169
Xojiakbar_9 <xojiakbar0719@gmail.com>
2026-07-06 17:58:36 +0000

    fix: use CI env variable to set correct github pages publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: process.env.NODE_ENV === 'production' ? '/ci-cd/' : '/', 
+    publicPath: process.env.CI ? '/ci-cd/' : '/', 
     clean: true,
     filename: '[name].[contenthash].js',
     assetModuleFilename: 'assets/[name][ext]',
